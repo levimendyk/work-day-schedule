2a784663fc2c4ea3aea0e002c06788e2bee0ee21
levimendyk <levi3743@gmail.com>
2022-07-24 17:29:25 -0600

    added a grid in html for the schedule block and psuedo coded TODOs in JavaScript

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// add current date to jumbotron (DDDD, Month, DD)
+// add text content (blank text fill section) when clicked
+// add gray to already past events
+// add color red to present event time
+// add green to future times
+// add blue log button
+// store text content to local storage
+// when refreshed the saved events persist
