12d30ccb6adb8c43cf69f945abcf73400488f994
levimendyk <levi3743@gmail.com>
2022-07-24 19:33:25 -0600

    Added some more size to the text area, added a MediaScreen to adjust text area when screen is smaller, aligned the schedule block

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 // add current date to jumbotron (DDDD, Month, DD)
 var currentDayDisplay = document.getElementById("currentDay");
+var hourPast;
+var hourCurrent;
+var hourFuture;
 
 currentDayDisplay.textContent = moment().format("dddd MMMM Do YYYY");
 
@@ -8,8 +11,8 @@ function inputText() {
   document.getElementsByClassName("textarea").value = "";
 }
 // add gray to already past events
+
 // add color red to present event time
 // add green to future times
-// add blue log button
 // store text content to local storage
 // when refreshed the saved events persist
